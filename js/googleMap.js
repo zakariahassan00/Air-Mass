8849742be656623d67908967c604665576830d5c
Zakaria Hassan <zakariahassan00@hotmail.com>
2019-03-18 03:55:28 +0200

    added google map

---
+++ js/googleMap.js
@@ -0,0 +1,7 @@
+// Google map
+function initMap() {
+  map = new google.maps.Map(document.getElementById("googleMap"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 
   $(window).on("scroll", function() {
     let pagePosition = window.scrollY > 120;
-    console.log(pagePosition);
     if (pagePosition) {
       $("nav").addClass("scrolled");
     } else {
