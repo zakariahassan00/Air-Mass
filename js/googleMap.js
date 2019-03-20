2e4e3c4ce44118fe18f9a61ea51b78ecfc49603b
Zakaria Hassan <zakariahassan00@hotmail.com>
2019-03-20 03:16:31 +0200

    fixed font size & fixed google map

--- js/googleMap.js
+++ js/googleMap.js
@@ -1,7 +1,14 @@
 // Google map
 function initMap() {
+  const myLatLng = { lat: 30.012083, lng: 31.429743 };
   map = new google.maps.Map(document.getElementById("googleMap"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    center: myLatLng,
+    zoom: 12
+  });
+
+  const marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: "Air Mass"
   });
 }
--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
     center: true,
     items: 1,
     loop: true,
-    margin: 10,
     autoplay: true,
     autoplayTimeout: 4000,
     // autoplayHoverPause: true,
